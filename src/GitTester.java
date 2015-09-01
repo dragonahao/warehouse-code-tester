e24d58cb0303611578393ef33112f77df7929b30
dragonahao <790785101@qq.com>
2015-09-02 07:16:58 +0800

    初次提交

---
+++ src/GitTester.java
@@ -0,0 +1,16 @@
+
+public class GitTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("--test github--");
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+		System.out.println("d");
+	}
+
+}

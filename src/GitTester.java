02186d2a70ccc65b4d4cc20ff95edfbd1a69c42a
Dragonahao <790785101@qq.com>
2015-09-02 08:38:42 +0800

    能使用第三方工具进行merge 和comp么？
    能使用第三方工具进行merge 和comp么？
    比如bcomp.exe

--- src/GitTester.java
+++ src/GitTester.java
@@ -12,11 +12,11 @@ public class GitTester {
 		//新增输出c
 		System.out.println("c");
 		
-	 //啊打算发送地方
+ 啊打算发送地方
 	
-	 //syso("不强制提交该如何一步一步的操作呢？");	
+	 syso("不强制提交该如何一步一步的操作呢？");	
 		
-	 // 撒旦发的
+	  撒旦发的
 		
 	// Git中 local file workspace是可以进行修改的
 		System.out.println("瞎写");

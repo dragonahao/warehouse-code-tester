e6feb4133e40017abcaa8c4b035057d5bd5d2249
dragonahao <790785101@qq.com>
2015-09-03 21:56:35 +0800

    branch中修改文件，提交到本地

--- src/ExperimentTester.java
+++ src/ExperimentTester.java
@@ -6,7 +6,10 @@ public class ExperimentTester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("branch"); 
+		System.out.println("A-branch");
+		System.out.println("branch");
+		System.out.println("a-branch changed");
+		
 	}
 
 }

2a5a1de9ec8fc78b2151a84a0eb3ca778c76fd17
dragonahao <790785101@qq.com>
2015-09-03 21:33:07 +0800

    分支中新增文件

--- src/GitTester.java
+++ src/GitTester.java
@@ -1,4 +1,3 @@
-
 public class GitTester {
 
 	/**
@@ -8,18 +7,8 @@ public class GitTester {
 		// TODO Auto-generated method stub
 		System.out.println("--test github--");
 		System.out.println("a");
-		System.out.println("b");
-		//新增输出c
-		System.out.println("c");
-		
- //啊打算发送地方
-	
-	 syso("不强制提交该如何一步一步的操作呢？");	
-		
-	  撒旦发的
-		
-	// Git中 local file workspace是可以进行修改的
-		System.out.println("瞎写");
+		System.out.println("b");  
+		System.out.println("E");
 	}
 
 }
